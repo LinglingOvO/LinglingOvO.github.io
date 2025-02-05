f596845ed8483a3a9d9ff5c9a1fc85e590ad5fbe
unknown <z1494612420@163.com>
2025-02-05 18:19:43 +0800

    Site updated: 2025-02-05 18:19:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/02/hello-world/","2025/02/02/test/","2025/02/02/数据类型/","2025/02/04/整数反转/"];function toRandomPost(){
+var posts=["2025/02/02/数据类型/","2025/02/02/test/","2025/02/02/hello-world/","2025/02/04/整数反转/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
