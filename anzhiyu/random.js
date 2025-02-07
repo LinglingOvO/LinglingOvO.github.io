08daf6fb07a9c34764f9971113cc04265b468aae
unknown <z1494612420@163.com>
2025-02-08 03:24:27 +0800

    Site updated: 2025-02-08 03:24:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/02/hello-world/","2025/02/02/数据类型/","2025/02/02/test/","2025/02/04/整数反转/"];function toRandomPost(){
+var posts=["2025/02/07/Cursor/","2025/02/02/test/","2025/02/02/hello-world/","2025/02/02/数据类型/","2025/02/04/整数反转/","2025/02/08/物理光学/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
