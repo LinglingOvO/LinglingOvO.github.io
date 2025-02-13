5d600f5f3e55464235a654e973674e780d90102e
unknown <z1494612420@163.com>
2025-02-13 19:53:13 +0800

    Site updated: 2025-02-13 19:53:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/10/hello-world/","2025/02/12/Markdown 语法学习/"];function toRandomPost(){
+var posts=["2025/02/12/Markdown 语法学习/","2025/02/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
