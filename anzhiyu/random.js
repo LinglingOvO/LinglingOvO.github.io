b2bc16568d53c2e7b577e584b978c9c90e2048ca
unknown <z1494612420@163.com>
2025-03-06 18:20:02 +0800

    Site updated: 2025-03-06 18:20:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/12/Markdown 语法学习/","2025/03/06/C++ 重修之旅（一）/","2025/02/15/Obsidian 软件学习/","2025/02/13/hello-world/"];function toRandomPost(){
+var posts=["2025/03/06/Cpp 重修之旅（一）/","2025/02/15/Obsidian 软件学习/","2025/02/12/Markdown 语法学习/","2025/02/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
