a7f434f57f5062b386e8d1a80ac21bcd9302c1b5
unknown <z1494612420@163.com>
2025-03-10 20:37:39 +0800

    Site updated: 2025-03-10 20:37:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/06/Cpp 重修之旅（一）/","2025/03/09/LeetCode 远征日记（1）/","2025/02/12/Markdown 语法学习/","2025/02/15/Obsidian 软件学习/","2025/02/13/hello-world/"];function toRandomPost(){
+var posts=["2025/03/09/LeetCode 远征日记（1）/","2025/03/06/Cpp 重修之旅（一）/","2025/03/10/LeetCode 远征日记（2）/","2025/02/12/Markdown 语法学习/","2025/02/13/hello-world/","2025/02/15/Obsidian 软件学习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
